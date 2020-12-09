49ee377531e8a529e62065d18f4eee4bcfe8759d
bensonlzl <bensonlinzl@gmail.com>
2020-12-09 03:04:59 +0000

    added cc api and MastHead component

---
+++ src/components/Masthead.js
@@ -0,0 +1,9 @@
+function MastHead(){
+    return(
+        <div className="mast">
+            Fatto Wabbito
+        </div>
+    );
+}
+
+export default MastHead;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ function Navbar() {
   return (
     <section className="nav">
       <nav className="container">
-        <span>GeekOut</span>
+        <span>GeekOut and Wabbit</span>
       </nav>
     </section>
   );
